0d5ccc1b4c2473b11c4d9a0c0c985719b7f4965b
ridhima1211 <69312874+ridhima1211@users.noreply.github.com>
2021-12-11 16:04:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyANx4jrODVYhz6MQert51uUidM55bLtGvw",
+  authDomain: "school-attendance-a-4c5ad.firebaseapp.com",
+  databaseURL: "https://school-attendance-a-4c5ad-default-rtdb.firebaseio.com",
+  projectId: "school-attendance-a-4c5ad",
+  storageBucket: "school-attendance-a-4c5ad.appspot.com",
+  messagingSenderId: "661800720984",
+  appId: "1:661800720984:web:79184515192cc3daaa082d"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
